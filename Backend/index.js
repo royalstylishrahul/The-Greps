3dc3d1a14917440f34e697e235e98716c32acf4a
designbypradeep <royalstylishrahul@Mac.lan>
2026-04-06 12:07:08 +0530

    fix: dotenv only for local environment

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 const serverless = require("serverless-http");
-require("dotenv").config();
+if(process.env.NODE_ENV !== "production"){
+  require("dotenv").config();
+}
 
 const express = require("express"); 
 const cors = require("cors");
