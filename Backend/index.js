7e99f20f2d2890082480c1a81b2aa15f8b81413d
designbypradeep <royalstylishrahul@Mac.lan>
2026-04-06 12:45:52 +0530

    cors fix

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
  ],
  credentials:true,
  methods:["GET","POST","PUT","DELETE","OPTIONS"],
- allowedHeaders:["Content-Type","Authorization"]
+ allowedHeaders:["Content-Type","Authorization","storeId"]
 }));
 
 app.options('*', cors());
